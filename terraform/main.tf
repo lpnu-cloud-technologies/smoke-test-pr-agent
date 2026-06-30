f7d7e97396bac66329e7956d15cf349335d01ad3
Anton Shakh <shakh@quintagroup.org>
2026-06-30 14:01:00 +0300

    lab-1: add authors DynamoDB table

--- terraform/main.tf
+++ terraform/main.tf
@@ -12,3 +12,14 @@ resource "aws_dynamodb_table" "courses" {
     type = "S"
   }
 }
+
+resource "aws_dynamodb_table" "authors" {
+  name         = "authors"
+  billing_mode = "PAY_PER_REQUEST"
+  hash_key     = "id"
+
+  attribute {
+    name = "id"
+    type = "S"
+  }
+}
